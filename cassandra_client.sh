6d34a91e35dec55e50bd0550a805216e70a294e3
mersault <mersault@varese.localdomain>
2011-08-29 20:38:59 -0500

    Some minor cleanup: 1. Moving the test of the Clojure Avro functions into a test directory 2. Removing the unused base Ruby test of Avro 3. Removing an unused shell script wrapper

--- cassandra_client.sh
+++
@@ -1,9 +0,0 @@
-# Shortcut wrapper to run thrift_cassandra_client
-
-# Make sure to include fencepost Clojure libs
-CLASSPATH=src/clojure
-
-# Re-use JARs included in the Cassandra distribution
-CLASSPATH=$CLASSPATH:~/local/cassandra/lib/apache-cassandra-thrift-0.8.2.jar:~/local/cassandra/lib/libthrift-0.6.jar:~/local/cassandra/lib/slf4j-api-1.6.1.jar:~/local/cassandra/lib/slf4j-log4j12-1.6.1.jar:~/local/cassandra/lib/log4j-1.2.16.jar
-
-~/local/bin/clojure src/clojure/thrift_cassandra_client.clj
